fe71cc5ab71e2e818c309cc76244f1b53b1e4509
lavya77 <lavya203@gmail.com>
2026-03-02 13:27:15 +0530

    file to look inside your computers and directories

---
+++ Automation/file_ops.py
@@ -0,0 +1,9 @@
+import os
+
+path = os.getcwd() # it gets you current working directory (path of it)
+
+print(path)
+
+file = os.listdir(path) # shows the direcotries in list, basically all the files it has shows you in form of list
+
+print(file)
